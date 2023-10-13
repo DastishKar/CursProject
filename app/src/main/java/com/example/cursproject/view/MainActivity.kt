09393e8050314017f7a03824a01ac957a6a24055
Dastan <karbayevd@gmail.com>
2023-10-13 16:57:23 +0600

    Main  logic finished

--- app/src/main/java/com/example/cursproject/view/MainActivity.kt
+++ app/src/main/java/com/example/cursproject/view/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cursproject.view
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.DatePicker
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cursproject.adapter.MainAdapter
 import com.example.cursproject.databinding.ActivityMainBinding
 import com.example.cursproject.mainInterface.MainApi
+import com.example.cursproject.viewModel.MainViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,27 +22,108 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MainAdapter
     private lateinit var mainApi: MainApi
+    private lateinit var datePickerDialog: DatePickerDialog
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        showFun() // Для методов retrofit and recycler
 
-    }
-
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
 
-
-
-    private fun showFun(){
-        initRecycler()
         initRetrofit()
+        initRecycler()
+        viewModelFun()
+        binding.apply {
+            categorySpinner.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener{
+                    override fun onItemSelected(
+                        parent: AdapterView<*>?,
+                        view: View?,
+                        position: Int,
+                        id: Long
+                    ) {
+                        val selectedCategory = parent?.getItemAtPosition(position).toString()
+                        viewModel.onCitySelected(selectedCategory)
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+
+        }
+        fun showDatePicker() {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            datePickerDialog = DatePickerDialog(
+                this@MainActivity,
+                { _: DatePicker, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+                    val selectedDate = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+                        .format(Calendar.getInstance().apply {
+                            set(Calendar.YEAR, selectedYear)
+                            set(Calendar.MONTH, selectedMonth)
+                            set(Calendar.DAY_OF_MONTH, selectedDay)
+                        }.time)
+                    selectedDate.also {
+                        binding.apply {
+                            dateTextView.text = it
+                            dateTextView.textSize = 20f
+                        }
+                    }
+                },
+                year, month, day
+            )
+            datePickerDialog.show()
+        }
+
+        binding.apply {
+            calendarButton.setOnClickListener {
+                showDatePicker()
+            }
+
+            saveButton.setOnClickListener {
+
+            }
+        }
+
     }
+
+
+
+    private fun viewModelFun(){
+        viewModel.city.observe(this@MainActivity, Observer{cities ->
+            val adapter = ArrayAdapter(
+                this@MainActivity,
+                android.R.layout.simple_spinner_item,
+                cities
+            )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.categorySpinner.adapter = adapter
+        })
+
+        viewModel.selectedDate.observe(this@MainActivity, Observer { selectedDate ->
+            "Выбранная дата: ${selectedDate.also { binding.dateTextView.text = it }}"
+        })
+
+
+    }
+
+
 
     private fun initRecycler(){
         binding.apply {
